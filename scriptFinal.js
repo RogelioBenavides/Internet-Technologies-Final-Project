5eb5e811bdc1f487d221f9c4abe279cb4b1ea920
Fabi <66051462+FabiChan@users.noreply.github.com>
2022-11-28 21:48:10 -0600

    Update scriptFinal.js
    JS actualizado

--- scriptFinal.js
+++ scriptFinal.js
@@ -1,20 +1,37 @@
+//Sección FAQ - acordeón
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+//Por cada elemento del acordeon, al hacer click, se despliega la información.
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
 //Función para cargar el popup del aviso de cookies
 window.addEventListener("load", function() {
-    setTimeout(
-      function open(event) {
-        //mostrar el div popup
-        document.querySelector(".popup1").style.display = "block";
-      },
-      500
-    )
-  });
-  
-  //Función: Si hace click en cerrar, se quita el popup
-  document.querySelector("#close").addEventListener ("click", function(){
-    document.querySelector(".popup1").style.display = "none";
-  });
-  
-  //Función: Si hace click en ACEPTAR, se quita el popup
-  document.querySelector(".popup1 a").addEventListener ("click", function(){
-    document.querySelector(".popup1").style.display = "none";
-  });
+  setTimeout(
+    function open(event) {
+      //mostrar el div popup
+      document.querySelector(".popup1").style.display = "block";
+    },
+    500
+  )
+});
+
+//Función: Si hace click en cerrar, se quita el popup
+document.querySelector("#close").addEventListener ("click", function(){
+  document.querySelector(".popup1").style.display = "none";
+});
+
+//Función: Si hace click en ACEPTAR, se quita el popup
+document.querySelector(".popup1 a").addEventListener ("click", function(){
+  document.querySelector(".popup1").style.display = "none";
+});
