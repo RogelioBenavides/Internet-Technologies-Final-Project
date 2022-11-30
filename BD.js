44cd2d6c15ad5888100ebb76638b1aafcc40aba4
Paredes223 <60049664+Paredes223@users.noreply.github.com>
2022-11-29 23:06:58 -0600

    Update BD.js

--- BD.js
+++ BD.js
@@ -1,46 +1,40 @@
+//configuracion de bd
 const firebaseConfig = {
-  //   copy your firebase config informations
+  apiKey: "AIzaSyDon15MRaZEBBiiMZpuNPf-n_KZj5kbsus",
+  authDomain: "ti-proyectofinal-1a056.firebaseapp.com",
+  databaseURL: "https://ti-proyectofinal-1a056-default-rtdb.firebaseio.com",
+  projectId: "ti-proyectofinal-1a056",
+  storageBucket: "ti-proyectofinal-1a056.appspot.com",
+  messagingSenderId: "1043128978228",
+  appId: "1:1043128978228:web:25a7cef4212f17ba9a7361",
+  measurementId: "G-3CVTMNQJP6"
 };
 
-// initialize firebase
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+const db=firebase.firestore();
+var a;
 
-// reference your database
-var contactFormDB = firebase.database().ref("contactForm");
-
-document.getElementById("contactForm").addEventListener("submit", submitForm);
-
-function submitForm(e) {
-  e.preventDefault();
-
-  var name = getElementVal("name");
-  var emailid = getElementVal("emailid");
-  var msgContent = getElementVal("msgContent");
-
-  saveMessages(name, emailid, msgContent);
-
-  //   enable alert
-  document.querySelector(".alert").style.display = "block";
-
-  //   remove the alert
-  setTimeout(() => {
-    document.querySelector(".alert").style.display = "none";
-  }, 3000);
-
-  //   reset the form
-  document.getElementById("contactForm").reset();
+function submitForm() {
+	var cont1 = document.getElementById("inputPassword1").value;
+	var cont2 = document.getElementById("inputPassword2").value;
+	if(cont1==cont2){
+		
+	  var correo = document.getElementById("inputEmail1").value;
+	  var nombre = document.getElementById("inputNombre").value;
+	  var apellido = document.getElementById("inputApellido").value;
+	  guardarDatos(correo, nombre, apellido, cont1);
+	  document.getElementById("registroCuenta").reset();
+	  
+	}
 }
 
-const saveMessages = (name, emailid, msgContent) => {
-  var newContactForm = contactFormDB.push();
+function guardarDatos(correom, nombrem, apellidom, contraseniam){
 
-  newContactForm.set({
-    name: name,
-    emailid: emailid,
-    msgContent: msgContent,
-  });
-};
-
-const getElementVal = (id) => {
-  return document.getElementById(id).value;
-};
+  db.collection("cuentas").add({
+    correo: correom,
+    nombre: nombrem,
+    apellido: apellidom,
+	contrasenia: contraseniam,
+  })
+}
